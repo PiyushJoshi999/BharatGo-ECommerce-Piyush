d7bf3e6601b0079be94e1d8f67d22b91842da373
PiyushJoshi999 <piyushjoshi470@gmail.com>
2025-01-08 18:48:33 +0530

    Added authentication functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./authentication/SignUp";
 import Login from "./authentication/Login";
 import Navbar from "./components/Navbar";
